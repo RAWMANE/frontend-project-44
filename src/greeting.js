88b7b0731ca1f7c30c73904f4cb38601fded36e5
RAWMANE <r.romanchyuk@gmail.com>
2023-07-13 16:27:49 +0300

    changes8

--- src/greeting.js
+++ src/greeting.js
@@ -2,11 +2,11 @@ import greetingAndAskName from './cli.js';
 
 const hello = (description = null) => {
   console.log('Welcome to the Brain Games!');
-  const userName = greetingAndAskName();
-  console.log(`Hello, ${userName}!`);
+  const name = greetingAndAskName();
+  console.log(`Hello, ${name}!`);
   if (description !== null) {
     console.log(description);
   }
-  return userName;
+  return name;
 };
 export default hello;
